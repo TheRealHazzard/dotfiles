9d83874136dca284a3269ad0e6ffa3ca2d18d222
Saksham Sethi <sakshamsethi@Sakshams-MacBook-Pro.local>
2021-01-30 15:22:20 +0530

    fix: Fixed echo appends and added installation status

--- homebrew.sh
+++ homebrew.sh
@@ -1,10 +1,10 @@
 # Install XCode commandline tools
-
+echo "Installing XCode CommandLine Tools"
 xcode-select —-install
 
 # ZSH is installed on macOS by default, 
 # Install oh-my-zsh
-
+echo "Setting-up ZSH!"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Then, install powerlevel10k ZSH theme
@@ -13,43 +13,42 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 # Set ZSH current theme to powerlevel10k or use my .zshrc
 
-echo ZSH_THEME="powerlevel10k/powerlevel10k" > ~/.zshrc.
-
-# Now reload zsh
-
-zsh
+echo ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc.
 
 # Let's now install Homebrew - package manager for macOS. (Don't worry, macOS comes pre-installed with ruby and python )
-
+echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Default macOS terminal is great but iTerm 2 is just phenomenal!
-
+echo "Installing iTerm2"
 brew install --cask iterm2
 
 # Cool! Now lets install the best editor in the world AKA Visual Studio Code
-
+echo "Installing Visual Studio Code"
 brew install --cask visual-studio-code
 
 # Now we will add autocompletion to our ZSH (Inspired from FISH)
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo plugins=(zsh-autosuggestions) > ~/.zshrc
+echo plugins=(zsh-autosuggestions) >> ~/.zshrc
 
 # Let's also add syntax highlighting to our ZSH
 
 brew install zsh-syntax-highlighting
-echo source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh > ~/.zshrc
+echo source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> ~/.zshrc
 
 # Now lets install some SDKs and Runtimes
 
 # -> Node.js
+echo "Installing Node.js"
 brew install node
 
 # -> Java
+echo "Intsalling OpenJDK 11"
 brew install openjdk@11
 
 # -> Go Lang
+echo "Installing GoLang"
 brew install go
 
 # Note: Clang already comes installed as xcode command line tools
@@ -57,18 +56,26 @@ brew install go
 # Let's now get some dev tools now, shall we?
 
 # -> Docker
+echo "Installing Docker Desktop"
 brew install --cask docker
 
 # -> MongoDB
+echo "Tapping MongoDB Community Edition"
 brew tap mongodb/brew
+echo "Installing MongoDB Community Edition"
 brew install mongodb-community
 
 # -> Redis DB
+echo "Installing RedisDB"
 brew install redis
 
 # Let's wrap up by installing JetBrains Toolbox
+echo "Installing JetBrains Toolbox"
 brew install --cask jetbrains-toolbox
 
+# Now reload zsh
+echo "Now reloading zsh..."
+zsh
 
 
 
